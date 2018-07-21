f4b97699eda480581afc9b32ba02ec35e9807a83
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-21 17:01:23 -0400

    added dataset, loaded and split dataset

--- dog-breed-id/breed-modelv2.py
+++ dog-breed-id/breed-modelv2.py
@@ -3,8 +3,6 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
-from os import listdir, makedirs
-from os.path import join, exists, expanduser
 from tqdm import tqdm
 from sklearn.metrics import log_loss, accuracy_score
 from keras.preprocessing import image
@@ -16,3 +14,27 @@ from keras.applications import inception_v3
 from sklearn.linear_model import LogisticRegression
 
 start = dt.datetime.now()
+
+INPUT_SIZE = 224
+NUM_CLASSES = 40
+SEED = 1987
+
+labels = pd.read_csv('dataset/kaggle/labels.csv')
+
+selected_breed_list = list(labels.groupby('breed').count().sort_values(by = 'id', ascending = False).head(NUM_CLASSES).index)
+
+labels['target'] = 1
+labels['rank'] = labels.groupby('breed').rank()['id']
+labels_pivot = labels.pivot('id', 'breed', 'target').reset_index().fillna(0)
+
+np.random.seed(seed = SEED)
+rand = np.random.random(len(labels))
+
+# split into training/validation sets
+train_index = rand < 0.8
+valid_index = rand >= 0.8
+y_train = labels_pivot[selected_breed_list].values
+
+ytr = y_train[train_index]
+yv = y_train[valid_index]
+
