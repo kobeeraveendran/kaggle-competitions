1f57bac2a06e14295023fb044aad09bc6ed4f430
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-21 04:00:52 -0400

    added imports

---
+++ dog-breed-id/breed-model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from os import listdir, makedirs
+from os.path import join, exists, expanduser
+from tqdm import tqdm
+from sklearn.metrics import log_loss, accuracy_score
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+from keras.applications import xception
+from keras.applications import inception_v3
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from sklearn.linear_model import LogisticRegression
+
+start = dt.datetime.now()
+
