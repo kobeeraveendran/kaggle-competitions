a1ab31957698659c152525de7121de3dab7d3af0
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-21 04:43:29 -0400

    added tensor path functions

--- dog-breed-id/breed-model.py
+++ dog-breed-id/breed-model.py
@@ -1,5 +1,9 @@
 import cv2
 import matplotlib.pyplot as plt
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from tqdm import tqdm
+import numpy as np
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
@@ -8,4 +12,17 @@ def face_detector(img_path):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     return len(faces) > 0
+
+ResNet50_model = ResNet50(weights = 'imagenet')
+
+def path_to_tensor(img_path):
+    img = image.load_img(img_path, target_size = (224, 224))
+    x = image.img_to_array(img)
+    
+    return np.expand_dims(x, axis = 0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+
+    return np.vstack(list_of_tensors)
 
