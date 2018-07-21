72dbdbd933c87dd20f5bd96086224856e685ca98
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-21 04:32:29 -0400

    restarted with tensorflow and keras on resnet50 model alone

--- dog-breed-id/breed-model.py
+++ dog-breed-id/breed-model.py
@@ -1,19 +1,11 @@
-import numpy as np
-import pandas as pd
-import datetime as dt
+import cv2
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
-from os import listdir, makedirs
-from os.path import join, exists, expanduser
-from tqdm import tqdm
-from sklearn.metrics import log_loss, accuracy_score
-from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
-from keras.applications.resnet50 import ResNet50
-from keras.applications import xception
-from keras.applications import inception_v3
-from keras.applications.vgg16 import preprocess_input, decode_predictions
-from sklearn.linear_model import LogisticRegression
 
-start = dt.datetime.now()
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
 
